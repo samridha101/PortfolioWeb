16afff7fdd16dddc3b95e8654466daeb422dee1f
Samridha <84956481+samridha101@users.noreply.github.com>
2025-03-27 01:24:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // Smooth scrolling
+    $("container mt-5").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 56
+            }, 800);
+        }
+    });
+
+    // Initialize Slick Carousel
+    $('.project-carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
